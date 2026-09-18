d7d9185f3a2439d3597e7d3af5d8cdee9a7f4d58
OhInteresting-droid <13904@edu.wheatlandhigh.org>
2026-09-18 17:52:10 +0000

    ok

---
+++ german.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Einstein {
+    public static void main(String[] args) {
+    Scanner gutenberg = new Scanner(System.in);
+    System.out.print("Enter Your Name: ");
+    String marx = gutenberg.nextLine();
+    marx[i] = gutenberg.nextLine();
+
+    }
+}
